0760e682bbeb9e0069f68cfcee3093731458d521
Panachai <panachai.ny@gmail.com>
2020-03-23 23:15:56 +0700

    Update GreeterService.cs

--- GrpcService101/Services/GreeterService.cs
+++ GrpcService101/Services/GreeterService.cs
@@ -10,9 +10,10 @@ namespace GrpcService101
     public class GreeterService : Greeter.GreeterBase
     {
         private readonly ILogger<GreeterService> _logger;
+
         public GreeterService(ILogger<GreeterService> logger)
         {
-                 _logger = logger;
+            _logger = logger;
         }
 
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
