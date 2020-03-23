6952215552da5648bbf5234792ce05e7702390e6
Panachai <panachai.ny@gmail.com>
2020-03-24 00:07:50 +0700

    implement gRPC to client

--- ConsoleAppClient/Program.cs
+++ ConsoleAppClient/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using GrpcService101;
+using System;
+using static GrpcService101.Greeter;
 
 namespace ConsoleAppClient
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new GreeterClient(channel);
+
+            var request = new HelloRequest
+            {
+                Name = "your name"
+            };
+
+            var helloReply = client.SayHello(request);
+            Console.WriteLine(helloReply.Message);
             Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
         }
     }
 }
